fea0e56fe1da38f77d1565e1767c9522a89c6024
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-11-15 20:32:43 -0700

    add error handlers a  server callbacks

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
 import express, { Request } from "express";
-import router from "./routes/index";
 import morgan from "morgan";
+import { auth } from "express-openid-connect";
+import { MongoError } from "mongodb";
+import pluralize from "pluralize";
 // import cookieParser from "cookie-parser";
 
-import { auth } from "express-openid-connect";
+import router from "./routes/index";
 import { ensureDBConnection } from "./middleware/db";
-import { MongoError } from "mongodb";
-
-const app = express();
+import path from "path";
 
 const config = {
   authRequired: true,
@@ -18,6 +18,9 @@ const config = {
   secret: process.env.AUTH0_CLIENT_SECRET,
 };
 
+const app = express();
+app.locals.pluralize = pluralize;
+app.use(express.static(path.join(__dirname, "public")));
 app.use(ensureDBConnection);
 
 // auth router attaches /login, /logout, and /callback routes to the baseURL
@@ -39,7 +42,6 @@ app
 
 // req.isAuthenticated is provided from the auth router
 app.get("/", async (req, res) => {
-  // @ts-ignore
   if (req.oidc.isAuthenticated()) {
     const { _raw, _json, ...userProfile } = (req as Request & { oidc: any })
       .oidc.user;
