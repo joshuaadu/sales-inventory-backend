7bc8890e5971be9acf91853ed177c5edf349be23
Joshua Adu-Mensah <joshuaadu2001@gmail.com>
2023-12-07 23:34:37 -0700

    nest service api in /api to decouple from frontend routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,8 @@
 import { Router } from "express";
 // import authRouter from "./auth";
-import productsRouter from "./products";
-import ordersRouter from "./orders";
-// import usersRouter from "./users.ts";
-// import cartRouter from "./cart.ts";
+
 import swaggerRouter from "./swagger";
+import apiRouter from "./api";
 // import productsRouter from "./products";
 
 const router = Router();
@@ -12,12 +10,16 @@ const router = Router();
 // Swagger
 // router.use(authRouter);
 router.use(swaggerRouter);
-router.use(productsRouter);
-router.use(ordersRouter);
+router.use("/api", apiRouter);
+
 // router.use(usersRouter);
 // router.use(cartRouter);
 router.get("/", (req, res) => {
   res.render("home", { pageTitle: "Home" });
+});
+
+router.get("/dashboard", (req, res) => {
+  res.render("dashboard", { pageTitle: "Dashboard" });
 });
 
 export default router;
